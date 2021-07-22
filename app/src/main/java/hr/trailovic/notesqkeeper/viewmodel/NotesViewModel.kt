9cfcb030f523af8154ec045e3adf2dfa29d9771c
Nikola Trailović <honeywhisperer3663@gmail.com>
2021-07-22 17:04:55 +0200

    fix notes view model

--- app/src/main/java/hr/trailovic/notesqkeeper/viewmodel/NotesViewModel.kt
+++ app/src/main/java/hr/trailovic/notesqkeeper/viewmodel/NotesViewModel.kt
@@ -1,8 +1,9 @@
 package hr.trailovic.notesqkeeper.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.*
-import androidx.lifecycle.Lifecycle.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import hr.trailovic.notesqkeeper.addOrRemove
 import hr.trailovic.notesqkeeper.model.Note
 import hr.trailovic.notesqkeeper.model.NoteSelectable
 import hr.trailovic.notesqkeeper.model.OneTimeEvent
@@ -11,131 +12,100 @@ import hr.trailovic.notesqkeeper.repo.NotesRepository
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+private const val TAG = ":::nVM"
+
 @HiltViewModel
 class NotesViewModel @Inject constructor(private val repo: NotesRepository) : ViewModel() {
 
-//    private var _allNotesMLD = MutableLiveData<List<NoteSelectable>>()
-//    val allNotesLD: LiveData<List<NoteSelectable>> = _allNotesMLD
+    /**Feed Main Activity with data*/
 
-    //  <<< Feed Main Activity with data
-    private val _notesFromDb: LiveData<List<Note>>
-        get() {//todo: fix
-            return repo.getAll()
-        }
+    private val _notesFromDb: LiveData<List<Note>> = repo.getAll()
 
     private var _selectedNotes = MutableLiveData<Set<Note>>()
 
     private var _merged = MediatorLiveData<List<NoteSelectable>>()
 
     val notesLD: LiveData<List<NoteSelectable>> = _merged
-    //  >>> Feed Main Activity with Notes data
 
-    // <<< Control switching between Add and Delete buttons visibilities
-    private var _addButtonVisible = MutableLiveData<Boolean>(_selectedNotes.value.isNullOrEmpty())
-    val addButtonVisible: LiveData<Boolean> = _addButtonVisible
-    // >>> Control switching between Add and Delete buttons visibilities
 
-    // <<< Open in AddActivity for detailed view / editing
+    /**Control switching between Add and Delete buttons visibilities*/
+
+    val addButtonVisible: LiveData<Boolean> = Transformations.switchMap(_selectedNotes) {
+        if (it.isNullOrEmpty())
+            return@switchMap MutableLiveData<Boolean>(true)
+        else
+            return@switchMap MutableLiveData<Boolean>(false)
+    }
+
+
+    /**Open in AddActivity for detailed view / editing*/
+
     private var _noteToEdit = MutableLiveData<OneTimeEvent<Note>>()
     val noteToEdit: LiveData<OneTimeEvent<Note>> = _noteToEdit
-    // >>> Open in AddActivity for detailed view / editing
+
 
     init {
-//        getAll()
+        /**_merged - Build LiveData sensitive to both: notes change and selection change*/
 
-        _merged.addSource(_notesFromDb) {
-//            val newList = merge(_notesFromDb.value, _selectedNotes.value)
-            val newList = merge(it, _selectedNotes.value)
+        _merged.addSource(_notesFromDb) { list ->
+            val newList = merge(list, _selectedNotes.value)
             _merged.postValue(newList)
         }
 
-        _merged.addSource(_selectedNotes) {
-//            val newList = merge(_notesFromDb.value, _selectedNotes.value)
-            val newList = merge(_notesFromDb.value, it)
+        _merged.addSource(_selectedNotes) { set ->
+            val newList = merge(_notesFromDb.value, set)
             _merged.postValue(newList)
         }
     }
-
-    private fun updateValues(notes: List<NoteSelectable>, selected: Set<Note>) {
-        notes.forEach {
-            it.isSelected = it.note in selected
-        }
-    }
-
-//    private fun getAll() {
-//        _notesFromDb = repo.getAll() as MutableLiveData<List<Note>>
-//    }
-
-//    fun add(title: String, body: String) {
-//        viewModelScope.launch {
-//            repo.add(Note(title, body))
-//        }
-//    }
 
 
     /**If set of selected notes is empty, show the current note in AddActivity.
     Otherwise, add or remove it from the selection.*/
     fun shortClick(note: NoteSelectable) {
         if (_selectedNotes.value.isNullOrEmpty()) {
-            // todo: open the note in another activity, todo: test
             _noteToEdit.postValue(note.note.toOneTimeEvent())
-
         } else {
-            changeIsSelectedStatus(note.note, _selectedNotes)
+            val newSet = changeIsSelectedStatus(note.note, _selectedNotes.value)
+            _selectedNotes.postValue(newSet)
         }
     }
 
 
     /**If no note is selected, add the current one to the selection;
     after this, every note that is shortClick-ed will be added or removed from the list of selected items.
-    (long clicks are the same as short)
     Selected items can be deleted - when set of selected items is not empty, Add button disappears and
     Delete button shows up.*/
-
     fun longClick(note: NoteSelectable) {
-        if (_selectedNotes.value.isNullOrEmpty()) {
-            changeIsSelectedStatus(note.note, _selectedNotes)
-        } else {
-            // noop
-
-            // todo
-            // ignore this option, for now;
-            // come up with some useful functionality later.
-        }
+        val newSet = changeIsSelectedStatus(note.note, _selectedNotes.value)
+        _selectedNotes.postValue(newSet)
     }
 
-    private fun changeIsSelectedStatus(note: Note, selection: MutableLiveData<Set<Note>>) {
-        selection.value?.let {
-            val setOfNotes = it as MutableSet
-            if (note in it) {
-                setOfNotes.remove(note)
-            } else {
-                setOfNotes.add(note)
-            }
-            selection.postValue(setOfNotes)
-        } ?: run {
-            selection.postValue(setOf(note))
-        }
+
+    /***/
+    private fun changeIsSelectedStatus(
+        note: Note,
+        selection: Set<Note>?
+    ): Set<Note> {
+        val setOfNotes = selection?.toMutableSet() ?: mutableSetOf()
+        setOfNotes.addOrRemove<Note>(note)
+        return setOfNotes
     }
 
-    /**
-     *
-     **/
-
+    /***/
     fun deleteSelected() {
         viewModelScope.launch {
             _selectedNotes.value?.let { set ->
                 set.forEach { note ->
                     repo.delete(note)
-                    val setOfNotes = set as MutableSet
-                    setOfNotes.remove(note)
-                    _selectedNotes.value = setOfNotes
                 }
+                _selectedNotes.postValue(emptySet())
             }
         }
     }
 
+    /***/
     private fun merge(notes: List<Note>?, statuses: Set<Note>?): List<NoteSelectable> {
+        Log.d(TAG, "merge: notes: ${notes?.size} set: ${statuses?.size}")
         val selectables = mutableListOf<NoteSelectable>()
         notes?.let { list ->
             list.forEach { note ->
